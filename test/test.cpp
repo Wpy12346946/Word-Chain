e676a4dd1ab6ae244bbf3903277153548589c8b5
Your Name <22369xxxx@qq.com>
2023-03-17 16:57:15 +0800

    增加测试数据

--- test/test.cpp
+++ test/test.cpp
@@ -127,7 +127,7 @@ void loadConfig(std::string &filepath, Config &config) {
 }
 
 TEST(test, TestMethod1) {
-    for (int i = 1; i <= 17; i++) {
+    for (int i = 1; i <= 22; i++) {
         coreTest(i);
     }
     for (int i = 1; i <= 14; i++) {
